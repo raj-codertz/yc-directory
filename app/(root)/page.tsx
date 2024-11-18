80aafd29bfb2a0f57a767bee5d92505edf964dad
rajabu shabani <rajabujshabani@gmail.com>
2024-11-18 13:11:43 +0300

    Done with startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
-import SearchForm from "../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
 
-export default async function Home({ searchParams}: {
-  searchParams: Promise<{ query?: string}>
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Rajabu Shabani'},
+      _id: 1,
+      description: "This is a dscription",
+      image:
+        "https://images.unsplash.com/photo-1667986292516-f27450ae75a9?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -16,6 +33,20 @@ export default async function Home({ searchParams}: {
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCradType, index: number) => <StartupCard key={post?._id} post={post}/>)
+          ) : (
+            <p className="no-results">No Startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
