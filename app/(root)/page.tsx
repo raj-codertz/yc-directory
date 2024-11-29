f22e8e5eeb7d6f790e4bdc8568d88fbc07c9196d
rajabu shabani <rajabujshabani@gmail.com>
2024-11-29 04:56:20 +0300

    Done: with real time search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,10 +11,11 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null}
 
   // Use client from sanity to fetch data from the query
   // const posts = await client.fetch(STARTUPS_QUERY);
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   // console.log(JSON.stringify(posts, null, 2));
 
